083aff4edaaa021a830055a6f96ebdd238a957b4
Andrew Yates <133114111+AndrewY3955@users.noreply.github.com>
2025-11-09 14:08:33 -0500

    Create AlertsSubject.java

---
+++ Subjects/AlertsSubject.java
@@ -0,0 +1,69 @@
+package Subjects;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Enums.Channel;
+import Observers.UIObserver;
+import javax.swing.JPanel;
+
+/**
+ * AlertsSubject sends alerts (strings) to observers.
+ */
+public class AlertsSubject extends JPanel implements UISubject {
+
+    private boolean isPush = true;
+    private List<UIObserver> observers;
+    private List<String> alerts;
+
+    public AlertsSubject() {
+        isPush = true;
+        observers = new ArrayList<>();
+        alerts = new ArrayList<>();
+    }
+
+    public void register(UIObserver o, Channel c) {
+        observers.add(o);
+    }
+
+    public void unregister(UIObserver o, Channel c) {
+        observers.remove(o);
+    }
+    
+    public void notifyObservers() {
+        UIUpdate currentState = new AlertsSubjectState(this);
+        if (isPush) {
+            for (UIObserver o : observers) {
+                o.update(currentState);
+            }
+        } else {
+            for (UIObserver o : observers) {
+                o.onNotified(this);
+            }
+        }
+    }
+
+    public void setMode(boolean isPush) {
+        this.isPush = isPush;
+    }
+
+    public AlertsSubjectState getState() {
+    return new AlertsSubjectState(this);
+    }
+
+    // Methods for MiniBadgeBar / other observers
+    public void addAlert(String alert) {
+        alerts.add(alert);
+        notifyObservers();
+    }
+
+    public void removeAlert(String alert) {
+        alerts.remove(alert);
+        notifyObservers();
+    }
+
+    // Return a copy of the alerts list to prevent external modification
+    public List<String> getAlerts() {
+        return new ArrayList<>(alerts);
+    }
+}
