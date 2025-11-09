eafdc7c0b9fd0066fa54c3d15f00e589ba30f517
Andrew Yates <133114111+AndrewY3955@users.noreply.github.com>
2025-11-09 12:23:27 -0500

    Create AirQualitySubjectState.java

---
+++ Subjects/AirQualitySubjectState.java
@@ -0,0 +1,19 @@
+package src.Subjects;
+
+public class AirQualitySubjectState implements UIUpdate {
+    private double airParticles;
+    private double ozone;
+
+    public AirQualitySubjectState(AirQualitySubject airQuality) {
+        this.airParticles = airQuality.getAirParticles();
+        this.ozone = airQuality.getOzone();
+    }
+
+    public double getAirParticles() {
+        return airParticles;
+    }
+
+    public double getOzone() {
+        return ozone;
+    }
+}
