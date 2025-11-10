73a258f2c8b517e209feaaf4ab27540e62e7b344
Thomas Dearth <twdearth@gmail.com>
2025-11-10 17:54:02 -0500

    Adds AirParticlesSlider using InputSlider

---
+++ Dashboard/InputSliders/AirParticlesSlider.java
@@ -0,0 +1,20 @@
+package InputSliders;
+import Subjects.AirQualitySubject;
+
+public class AirParticlesSlider extends InputSlider {
+    
+    // TODO: assign new values
+    private static final int minAP = 0;
+    private static final int maxAP = 20;
+    private static final int defaultAP = 5;
+
+    public AirParticlesSlider(AirQualitySubject observer) {
+        super(observer, minAP, maxAP, defaultAP, "Air Particle Concentration: ", " ppb");
+    }
+
+    public void updateField(int newAP) {
+        super.updateField(newAP);
+
+        ((AirQualitySubject) getSubject()).setAirParticles(newAP);
+    }
+}
