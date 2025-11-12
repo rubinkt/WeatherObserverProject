65017d5b4b5ad1c6676632e540d0c044862bfecd
Keith Rubin <keithrubin99@gmail.com>
2025-11-11 21:17:17 -0500

    adds most recent alert to badge bar

--- Observers/MiniBadgeBar.java
+++ Observers/MiniBadgeBar.java
@@ -2,6 +2,7 @@ package Observers;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 import Subjects.UIUpdate;
 import Subjects.UISubject;
@@ -10,6 +11,7 @@ import Subjects.AlertsSubjectState;
 
 public class MiniBadgeBar extends JPanel implements UIObserver {
     private int alertsReceived = 0;
+    private String mostRecentAlertMessage = "No alerts";
 
     public MiniBadgeBar() {
         setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
@@ -17,27 +19,34 @@ public class MiniBadgeBar extends JPanel implements UIObserver {
         setBorder(BorderFactory.createTitledBorder("Mini Badge Bar"));
         setPreferredSize(new Dimension(400, 60));
         alertsReceived = 0;
-        refreshCount();
+        mostRecentAlertMessage = "No alerts";
+        refreshPanel();
     }
 
     public void update(UIUpdate update) {
         if (update instanceof AlertsSubjectState) {
             alertsReceived++;
-            refreshCount();
+            ArrayList<String> alerts = ((AlertsSubjectState) update).getAlerts();
+            mostRecentAlertMessage = alerts.get(alerts.size() - 1);
+            refreshPanel();
         }
     }
 
     public void onNotified(UISubject subj) {
         if (subj instanceof AlertsSubject) {
             alertsReceived++;
-            refreshCount();
+            ArrayList<String> alerts = ((AlertsSubject) subj).getState().getAlerts();
+            mostRecentAlertMessage = alerts.get(alerts.size() - 1);
+            refreshPanel();
         }
-        }
+    }
 
-    private void refreshCount() {
+    private void refreshPanel() {
         removeAll();
         JLabel alertCount = new JLabel("" + alertsReceived);
+        JLabel recentAlert = new JLabel(mostRecentAlertMessage);
         add(alertCount);
+        add(recentAlert);
         revalidate();
         repaint();
     }
